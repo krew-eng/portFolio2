c950bcc3a5dfefab8443b5a1ca633c64fcd06968
Oksana Adamchuk <adamchuk.oksana01.08@gmail.com>
2021-11-27 18:06:18 +0200

    add animation to projects page

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -1,15 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+
 
 @Component({
   selector: 'app-project',
   templateUrl: './project.component.html',
-  styleUrls: ['./project.component.scss']
+  styleUrls: ['./project.component.scss'],
+  animations: [
+    trigger('animationRightSecond', [
+      transition('void => *', [
+        style({ position: 'relative',
+        right: '-70%' }),
+        animate('3s 4s ease-in-out', style({ right: 0 })), 
+      ]),
+    ]),
+    trigger('animationLeftFirst', [
+      transition('void => *', [
+        style({ position: 'relative',
+        left: '-70%' }),
+        animate('3s ease-in-out', style({ left: 0 })), 
+      ]),
+    ]),
+    trigger('animationRightFirst', [
+      transition('void => *', [
+        style({ position: 'relative',
+        right: '-70%' }),
+        animate('3s ease-in-out', style({ right: 0 })), 
+      ]),
+    ]),
+    trigger('animationLeftSecond', [
+      transition('void => *', [
+        style({ position: 'relative',
+        left: '-70%' }),
+        animate('3s 2s ease-in-out', style({ left: 0 })), 
+      ]),
+    ]),
+
+  ]
 })
-export class ProjectComponent implements OnInit {
+export class ProjectComponent implements OnInit, AfterViewInit{
+
+  public isVisible = false;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.isVisible = !this.isVisible;
+  }
+
+  ngAfterViewInit(): void {
+    
   }
 
 }
